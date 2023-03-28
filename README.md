117a0d40f226ba62e6980c607b1dd75dd4b18d71
Christoph <606567+monochromec@users.noreply.github.com>
2023-03-28 10:26:47 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # k8s
 FraLUG talk on k8s
+
+* main1.go: Simple HTTP server
+* main2.go: Improved HTTP server
+* depl[12].yml: Deployment YAMLs
+* svc[12].yml: Service YAMLs
+* build.sh: Create image and push to local registry
---
+++ main2.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+var sad = false
+var broken = false
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	// Set sad flag
+	sad = strings.Contains(strings.ToLower(name), "sad")
+	if strings.Contains(strings.ToLower(name), "kaputt") {
+		// Flip if received "kaputt"
+		broken = !broken
+	}
+	const MAX_LEN = 32
+	guest_name, _ := os.LookupEnv("HOSTNAME")
+	str_len := len(guest_name)
+	from := 0
+	if str_len > MAX_LEN {
+		from = str_len - MAX_LEN
+	}
+	log.Printf("Received request for %s\n", name)
+	w.Write([]byte(fmt.Sprintf("Hello, %s from container %s (sad: %t, broken %t)\n", name, guest_name[from:str_len], sad, broken)))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	var status int
+	if sad {
+		status = http.StatusInternalServerError
+	} else {
+		status = http.StatusOK
+	}
+	w.WriteHeader(status)
+	log.Printf("HealthHandler sent %d\n", status)
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	var status int
+	if broken {
+		status = http.StatusServiceUnavailable
+	} else {
+		status = http.StatusOK
+	}
+	w.WriteHeader(status)
+	log.Printf("ReadinessHandler sent %d\n", status)
+}
+
+func main() {
+	// Create Server and Route Handlers
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler)
+	r.HandleFunc("/health", healthHandler)
+	r.HandleFunc("/readiness", readinessHandler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Start Server
+	go func() {
+		log.Println("Starting Server")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Graceful Shutdown
+	waitForShutdown(srv)
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down")
+	os.Exit(0)
+}
